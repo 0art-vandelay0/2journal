18405df98013b245a85e33c49f81b7625592f006
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-20 16:30:28 -0700

    Fix teaser value output

--- src/index.js
+++ src/index.js
@@ -18,18 +18,18 @@ function handleSubmit(event) {
     const ptag2 = document.createElement('p');
     const consResponse = entry.countConsonants();
     const ptag3 = document.createElement('p');
-    // const teaser = entry.getTeaser();
-    // const ptag4 = document.createElement('p');
+    const teaser = entry.getTeaser();
+    const ptag4 = document.createElement('p');
 
     ptag.append("Word count: " + wordResponse);
     ptag2.append("Vowel count: " + vowelResponse);
     ptag3.append("Consonants count: " + consResponse);
-    // ptag4.append("Sentence Teaser: " + teaser);
+    ptag4.append("Sentence Teaser: " + teaser);
 
     document.querySelector('#entries').append(ptag);
     document.querySelector('#entries').append(ptag2);
     document.querySelector('#entries').append(ptag3);
-    // document.querySelector('#entries').append(ptag4);
+    document.querySelector('#entries').append(ptag4);
 }
 
 window.addEventListener("load", function() {
